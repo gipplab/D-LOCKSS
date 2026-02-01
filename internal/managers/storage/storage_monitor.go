c25e1b59cda9144b9faa88e713b316384937fb18
Cornelius Ihle <ihle.cornelius@gmail.com>
2026-02-01 22:33:07 +0000

    Actions runner to build bins (#18)
    * Actions: Disk usage is now implemented per OS with build tags

--- internal/managers/storage/storage_monitor.go
+++ internal/managers/storage/storage_monitor.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"log"
 	"sync"
-	"syscall"
 	"time"
 
 	"dlockss/internal/config"
 )
+
+// getDiskUsagePercent is implemented in storage_monitor_linux.go (Linux) and storage_monitor_stub.go (!linux).
 
 var (
 	diskUsage = struct {
@@ -20,25 +21,6 @@ var (
 		lastCheck:    time.Time{},
 	}
 )
-
-func getDiskUsagePercent(path string) (float64, error) {
-	var stat syscall.Statfs_t
-	err := syscall.Statfs(path, &stat)
-	if err != nil {
-		return 0, err
-	}
-
-	total := stat.Blocks * uint64(stat.Bsize)
-	available := stat.Bavail * uint64(stat.Bsize)
-	used := total - available
-
-	if total == 0 {
-		return 0, nil
-	}
-
-	usagePercent := float64(used) / float64(total) * 100.0
-	return usagePercent, nil
-}
 
 func CheckDiskUsage() float64 {
 	diskUsage.RLock()
